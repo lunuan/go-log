7dfadaa437efb1bd49d35010925b34a12172cace
LiuHX <huaxing@lunuan.com.cn>
2024-09-21 23:45:53 +0800

    first version complate

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/lunuan/log
+
+go 1.21.6
+
+require (
+	github.com/lunuan/gopkg v1.0.5
+	go.uber.org/zap v1.27.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
+
+require go.uber.org/multierr v1.11.0 // indirect
---
+++ log_test.go
@@ -0,0 +1,37 @@
+package log
+
+import "testing"
+
+func TestInitLogger(t *testing.T) {
+	c := &Config{
+		FilePath: "/tmp/test-init-logger.log",
+		Format:   "common",
+		Level:    "debug",
+	}
+	Init(c)
+
+	Debug("test debug")
+	Debugf("test debugf %s", "test")
+	Debugw("test debugw", "key", "value")
+
+	Info("test info")
+	Infof("test infof %s", "test")
+	Infow("test infow", "key", "value")
+
+	Warn("test warn")
+	Warnf("test warnf %s", "test")
+	Warnw("test warnw", "key", "value")
+
+	Error("test error")
+	Errorf("test errorf %s", "test")
+	Errorw("test errorw", "key", "value")
+
+	// Fatal("test fatal")
+	// Fatalf("test fatalf %s", "test")
+	// Fatalw("test fatalw", "key", "value")
+
+	// Panic("test panic")
+	// Panicf("test panicf %s", "test")
+	// Panicw("test panic", "key", "value")
+
+}
